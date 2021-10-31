ddbb46bc126d2cf4e0190db1f6a0313909a6592f
wConnor <connorga4@gmail.com>
2021-10-31 16:37:02 +0000

    fixed incorrect data types; char -> uintX_t + constexprs.

--- chip8.cpp
+++ chip8.cpp
@@ -2,11 +2,11 @@
 
 Chip8::Chip8() {
 	/* intialises the arrays with 0 */
-	std::fill(memory.begin(), memory.end(), 0x30);
-	std::fill(v_regs.begin(), v_regs.end(), 0x30);
-	std::fill(stack.begin(), stack.end(), 0x30);
-	std::fill(keys.begin(), keys.end(), 0x30);
-	std::fill(display.begin(), display.end(), 0x30);
+	std::fill(memory.begin(), memory.end(), 0x0);
+	std::fill(v_regs.begin(), v_regs.end(), 0x0);
+	std::fill(stack.begin(), stack.end(), 0x0);
+	std::fill(keys.begin(), keys.end(), 0x0);
+	std::fill(display.begin(), display.end(), 0x0);
 
 	char op_code = (memory[pc] << 8) | memory[pc + 1];
 	std::cout << "op_code: " << std::hex << op_code << std::endl;
--- chip8.hpp
+++ chip8.hpp
@@ -2,19 +2,25 @@
 #include <algorithm>
 #include <array>
 
+constexpr std::uint16_t MEMORY_SIZE = 4096;
+constexpr std::uint8_t STACK_SIZE = 16;
+constexpr std::uint8_t REGISTER_COUNT = 16;
+constexpr std::uint8_t KEY_COUNT = 16;
+constexpr std::uint16_t DISPLAY_SIZE = 16 * 32;
+
 class Chip8
 {
 private:
-	std::array <char, 4096> memory;
-	std::array <char, 16> v_regs;
-	char i_ptr = 0x0;
-	int pc = 0x200;
-	std::array <char, 16> stack;
-	int stack_ptr = 0;
-	int delay_timer = 0;
-	int sound_timer = 0;
-	std::array <char, 16> keys;
-	std::array <char, 16 * 32> display;
+	std::array <uint8_t, MEMORY_SIZE> memory;
+	std::array <uint8_t, REGISTER_COUNT> V; /* data registers: V0 to VF */
+	uint16_t I = 0x0; /* address register */
+	uint16_t pc = 0x200;
+	std::array <uint8_t, STACK_SIZE> stack;
+	std::uint8_t stack_ptr = 0;
+	std::uint8_t delay_timer = 0;
+	std::uint8_t sound_timer = 0;
+	std::array <uint8_t, KEY_COUNT> keys;
+	std::array <uint8_t, DISPLAY_SIZE> display;
 
 public:
 	Chip8();
